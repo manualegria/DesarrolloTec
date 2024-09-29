5bc2e8fd494586164f9fb60f7116e0d48f562c49
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-09-29 17:56:41 -0500

    feat: se crean controladores para servicios, empleados y proyectos

---
+++ DesarrolloTec.API/Controllers/ServicesController.cs
@@ -0,0 +1,89 @@
+﻿using DesarrolloTec.API.Data;
+using DesarrolloTec.Shered.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesarrolloTec.API.Controllers
+{
+   
+     [ApiController]
+     [Route("/api/services")]
+
+     public class ServiceController : ControllerBase
+     {
+            private readonly DataContext _context;
+
+            public ServiceController(DataContext context)
+            {
+                _context = context;
+            }
+
+            [HttpGet]
+            public async Task<ActionResult> Get()
+            {
+              return Ok(await _context.Services.ToArrayAsync());
+            }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var Services = await _context.Services.FirstOrDefaultAsync( x => x.Id == id);
+
+            if (Services == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(Services);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Service service)
+        {
+            _context.Services.Add(service);
+            await _context.SaveChangesAsync();
+            return Ok(new
+            {
+                message = "Servicio creado con éxito.",
+                data = service
+            });
+        }
+
+        [HttpPut]
+
+        public async Task<ActionResult> Put(Service service)
+        {
+            _context.Services.Update(service);
+            await _context.SaveChangesAsync();
+            return Ok(new
+            {
+                message = "Servicio actualizado con éxito.",
+                data = service
+            });
+
+        }
+
+        [HttpDelete("{id:int}")]
+
+        public async Task<ActionResult> Delete(int id )
+        {
+            var delete = await _context.Services
+                .Where( x => x.Id == id )
+            .ExecuteDeleteAsync();
+
+            if(delete == 0)
+            {
+                return NotFound(new { message = "Servicio no encontrado." });
+
+            }
+            else
+            {
+                return Ok(new { message = "Servicio eliminado con éxito." });
+            }
+        }
+     }
+   
+    
+}
