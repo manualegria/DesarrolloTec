798f1b5c1dcb00a3d107938b3722e860bc927c5a
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-10-11 22:48:57 -0500

    feat: se crea archivo seedDB para el llenado en base de datos

---
+++ DesarrolloTec.API/Data/SeedDb.cs
@@ -0,0 +1,44 @@
+﻿using DesarrolloTec.Shered.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesarrolloTec.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            // va a la base de datos y asegurese que sean los datos que le estoy enviando y guardelos
+            await _context.Database.EnsureCreatedAsync();
+
+            await CheckServiceAsync();
+        }
+
+
+        public async Task CheckServiceAsync()
+        {
+            if (!_context.Services.Any())
+            {
+                _context.Services.Add(new Service 
+                { 
+                    Name = "Asesoria",
+                    Description = "Asesoria para almacenamiento en la nube",
+                    EstimatedPrice = 500000
+
+
+                });
+             
+
+            }
+            await _context.SaveChangesAsync();
+
+        }
+    }
+}
