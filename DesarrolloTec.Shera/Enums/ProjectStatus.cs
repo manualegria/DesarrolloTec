1b62db23960fcdde1ee0c027549568b43183e943
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-11-07 22:54:37 -0500

    feath: creacion de enums para manejp de estados

---
+++ DesarrolloTec.Shera/Enums/ProjectStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesarrolloTec.Shared.Enums
+{
+    public enum ProjectStatus
+    {
+
+        Pending,
+        InProgress,
+        Completed,
+        OnHold,
+        Canceled
+    }
+}
---
+++ DesarrolloTec.Shera/Enums/TaskStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesarrolloTec.Shared.Enums
+{
+    public enum TaskStatus
+    {
+        nitialized,
+        Ongoing,
+        Completed
+    }
+}
