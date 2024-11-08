6ec410628a5aaf02731cb75b84b7f36a96ea5a65
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-11-07 23:29:55 -0500

    fix: inyeccion de dependencias

--- DesarrolloTec.WEB/Program.cs
+++ DesarrolloTec.WEB/Program.cs
@@ -1,11 +1,15 @@
 using DesarrolloTec.WEB;
+using DesarrolloTec.WEB.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using CurrieTechnologies.Razor.SweetAlert2;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7001") });
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
