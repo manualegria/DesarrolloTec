b98bea04d5a17395971c88ff7e7f986963efb55f
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-11-21 23:03:33 -0500

    feat: se crea clase e interfaz para manejar los metos de asignacion de roles a slos usuarios?

---
+++ DesarrolloTec.API/Helpers/IUserhelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Veterinary.Shared.DTOs;
+using Veterinary.Shared.Entities;
+
+namespace DesarrolloTec.API.Helpers
+{
+    public interface IUserhelper
+    {
+        Task<User> GetUserAsync(string email);
+
+        Task<IdentityResult> AddUserAsync(User user, string password);
+
+        Task CheckRoleAsync(string roleName);
+
+        Task AddUserToRoleAsync(User user, string roleName);
+
+        Task<bool> IsUserInRoleAsync(User user, string roleName);
+
+        Task<SignInResult> LoginAsync(LoginDTO model);
+
+        Task LogoutAsync();
+
+        // Implementación del método GetAllUsersAsync
+         Task<List<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync(string id); // Nuevo método
+        Task<IdentityResult> DeleteUserAsync(User user); // Nuevo métod
+        Task<IdentityResult> UpdateUserAsync(User user);
+        Task<IList<string>> GetRolesAsync(User user); // Nuevo método
+        Task<IdentityResult> RemoveFromRolesAsync(User user, IEnumerable<string> roles); // Nuevo método
+
+    }
+}
---
+++ DesarrolloTec.API/Helpers/UserHelper.cs
@@ -0,0 +1,109 @@
+﻿using DesarrolloTec.API.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Veterinary.Shared.DTOs;
+using Veterinary.Shared.Entities;
+
+namespace DesarrolloTec.API.Helpers
+{
+    public class UserHelper : IUserhelper
+    {
+        private readonly DataContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public UserHelper(DataContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager, SignInManager<User> signInManager)
+        {
+
+
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+
+
+        }
+
+
+        //Crear un nuevo usuario
+        public async Task<IdentityResult> AddUserAsync(User user, string password)
+        {
+            return await _userManager.CreateAsync(user, password);
+        }
+
+        public async Task AddUserToRoleAsync(User user, string roleName)
+        {
+            await _userManager.AddToRoleAsync(user, roleName);
+        }
+
+        public async Task CheckRoleAsync(string roleName)
+        {
+            bool roleExists = await _roleManager.RoleExistsAsync(roleName);
+            if (!roleExists)
+            {
+                await _roleManager.CreateAsync(new IdentityRole
+                {
+                    Name = roleName
+                });
+            }
+        }
+
+        public async Task<User> GetUserAsync(string email)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+        public async Task<bool> IsUserInRoleAsync(User user, string roleName)
+        {
+            return await _userManager.IsInRoleAsync(user, roleName);
+        }
+
+
+        public async Task<SignInResult> LoginAsync(LoginDTO model)
+        {
+
+            return await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+
+        }
+
+        public async Task LogoutAsync()
+        {
+
+            await _signInManager.SignOutAsync();
+
+        }
+
+        // Implementación del método GetAllUsersAsync
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            return await _userManager.Users.ToListAsync();  // Obtiene todos los usuarios desde la base de datos
+        }
+
+        public async Task<User> GetUserByIdAsync(string id)
+        {
+            return await _userManager.FindByIdAsync(id);
+        }
+
+        public async Task<IdentityResult> DeleteUserAsync(User user)
+        {
+            return await _userManager.DeleteAsync(user);
+        }
+
+        public async Task<IdentityResult> UpdateUserAsync(User user)
+        {
+            return await _userManager.UpdateAsync(user);
+        }
+
+        public async Task<IList<string>> GetRolesAsync(User user)
+        {
+            return await _userManager.GetRolesAsync(user);
+        }
+
+        public async Task<IdentityResult> RemoveFromRolesAsync(User user, IEnumerable<string> roles)
+        {
+            return await _userManager.RemoveFromRolesAsync(user, roles);
+        }
+    }
+}
