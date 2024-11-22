1631a37cd8637b6ea872e40f7f325eb6c6d4518c
Manuel Emir Murillo Alegría <160237709+manualegria@users.noreply.github.com>
2024-11-22 04:41:18 -0500

    Update ProjectServiceController.cs

--- DesarrolloTec.API/Controllers/ProjectServiceController.cs
+++ DesarrolloTec.API/Controllers/ProjectServiceController.cs
@@ -1,5 +1,4 @@
 ﻿using DesarrolloTec.API.Data;
-<<<<<<< HEAD
 using DesarrolloTec.Shared.Entities;
 using DesarrolloTec.Shered.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -14,7 +13,7 @@ namespace DesarrolloTec.API.Controllers
 
     [ApiController]
     [Route("/api/projectServices")]
-=======
+
 using DesarrolloTec.Shered.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -23,34 +22,26 @@ using System.Security.AccessControl;
 
 namespace DesarrolloTec.API.Controllers
 {
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
+
     public class ProjectServiceController : ControllerBase
     {
 
         private readonly DataContext _context;
-<<<<<<< HEAD
-=======
 
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
         public ProjectServiceController(DataContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-
-<<<<<<< HEAD
         public async Task<IActionResult> Get()
         {
-<<<<<<< HEAD
             return Ok(await _context.ProjectService.ToListAsync());
         }
-=======
         //public async Task<IActionResult> Get()
         //{
         //    return Ok(await _context.ProjectService.ToListAsync());
         //}
->>>>>>> emirDev
 
         public async Task<List<ProjectService>> GetInvoicesAsync()
         {
@@ -60,16 +51,13 @@ namespace DesarrolloTec.API.Controllers
                 .ToListAsync();
         }
 
-=======
             return Ok(await _context.ProjectServices.ToListAsync());
         }
-
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
         [HttpGet("{id:int}")]
 
         public async Task<IActionResult> Get(int id)
         {
-<<<<<<< HEAD
+
             var projectservices = await _context.ProjectService.SingleOrDefaultAsync(x => x.Id == id);
 
             if (projectservices == null)
@@ -80,7 +68,7 @@ namespace DesarrolloTec.API.Controllers
             else
             {
                 return Ok(projectservices);
-=======
+
             var projectservice = await _context.ProjectServices.SingleOrDefaultAsync(x => x.Id == id);
 
             if (projectservice == null)
@@ -90,13 +78,10 @@ namespace DesarrolloTec.API.Controllers
             else
             {
                 return Ok(projectservice);
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
             }
         }
 
         [HttpPost]
-
-<<<<<<< HEAD
         public async Task<IActionResult> Post(ProjectService projectservices)
         {
             _context.ProjectService.Add(projectservices);
@@ -105,7 +90,7 @@ namespace DesarrolloTec.API.Controllers
             {
                 message = "creado con éxito.",
                 data = projectservices
-=======
+
         public async Task<IActionResult> Post(ProjectService projectservice)
         {
             _context.ProjectServices.Add(projectservice);
@@ -115,13 +100,11 @@ namespace DesarrolloTec.API.Controllers
             {
                 message = "Servicio integrado al proyecto creado con éxito.",
                 data = projectservice
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
             });
         }
 
         [HttpPut]
 
-<<<<<<< HEAD
         public async Task<IActionResult> Put(ProjectService projectservices)
         {
             _context.ProjectService.Update(projectservices);
@@ -130,7 +113,7 @@ namespace DesarrolloTec.API.Controllers
             {
                 message = " Actualizado con éxito.",
                 data = projectservices
-=======
+
         public async Task<IActionResult> Put(ProjectService projectservice)
         {
             _context.ProjectServices.Add(projectservice);
@@ -140,7 +123,6 @@ namespace DesarrolloTec.API.Controllers
             {
                 message = "Servicio Actualizado al proyecto con éxito.",
                 data = projectservice
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
             });
         }
 
@@ -148,7 +130,7 @@ namespace DesarrolloTec.API.Controllers
 
         public async Task<IActionResult> Delete(int id)
         {
-<<<<<<< HEAD
+
             var Delete = await _context.ProjectService
                 .Where(x => x.Id == id).ExecuteDeleteAsync();
 
@@ -160,7 +142,7 @@ namespace DesarrolloTec.API.Controllers
             else
             {
                 return Ok(new { message = "Registro eliminado con éxito." });
-=======
+
             var projectserviceDelete = await _context.ProjectServices.
                 Where(x => x.Id == id)
                 .ExecuteDeleteAsync();
@@ -172,7 +154,7 @@ namespace DesarrolloTec.API.Controllers
             else
             {
                 return Ok(new { message = "Servicio del proyecto eliminado con éxito.", data = projectserviceDelete });
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
+
             }
         }
     }
