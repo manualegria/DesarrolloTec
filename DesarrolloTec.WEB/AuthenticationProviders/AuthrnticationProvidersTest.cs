1344a63295de86a1d8e71ca8c781e1b820da26f5
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-11-21 23:15:16 -0500

    feat: autenticacion por medio del JWT

---
+++ DesarrolloTec.WEB/AuthenticationProviders/AuthrnticationProvidersTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+
+namespace DesarrolloTec.WEB.AuthenticationProviders
+{
+    public class AuthrnticationProvidersTest : AuthenticationStateProvider
+    { 
+            public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+            {
+            await Task.Delay(3000);
+            var anonimous = new ClaimsIdentity();
+                return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+            }
+        
+    }
+}
