bb7aedc65bc40981045bee479da1a64d84d34afc
Manuel Emir Murillo Alegría <160237709+manualegria@users.noreply.github.com>
2024-11-22 04:43:20 -0500

    Update ResourceController.cs

--- DesarrolloTec.API/Controllers/ResourceController.cs
+++ DesarrolloTec.API/Controllers/ResourceController.cs
@@ -1,6 +1,5 @@
 ﻿using DesarrolloTec.API.Data;
 using DesarrolloTec.Shared.Entities;
-<<<<<<< HEAD
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -15,7 +14,7 @@ namespace DesarrolloTec.API.Controllers
     public class ResourceController : ControllerBase
     {
         private readonly DataContext _context;
-=======
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.AccessControl;
@@ -25,16 +24,12 @@ namespace DesarrolloTec.API.Controllers
     public class ResourceController : ControllerBase
     {
         private readonly DataContext _context;
-
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
         public ResourceController(DataContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-
-<<<<<<< HEAD
         public async Task<List<Resource>> GetInvoicesAsync()
         {
             return await _context.Resourcess
@@ -42,60 +37,43 @@ namespace DesarrolloTec.API.Controllers
                 .ToListAsync();
         }
 
-
-=======
         public async Task<IActionResult> Get()
         {
             return Ok(await _context.Resourcess.ToListAsync());
         }
 
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
         [HttpGet("{id:int}")]
 
         public async Task<IActionResult> Get(int id)
         {
-<<<<<<< HEAD
+
             var resource = await _context.Resourcess
             .Include(i => i.Projects)
             .SingleOrDefaultAsync(x => x.Id == id);
-=======
             var resource = await _context.Resourcess.SingleOrDefaultAsync(x => x.Id == id);
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
 
             if (resource == null)
             {
                 return NotFound();
-<<<<<<< HEAD
-
-=======
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
             }
             else
             {
                 return Ok(resource);
             }
         }
-<<<<<<< HEAD
-          
-=======
 
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
         [HttpPost]
 
         public async Task<IActionResult> Post(Resource resource)
         {
             _context.Resourcess.Add(resource);
             await _context.SaveChangesAsync();
-<<<<<<< HEAD
             return Ok(new
             {
                 message = "Recurso creado con éxito.",
-=======
-
             return Ok(new
             {
                 message = "Recurso creado con exito.",
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
                 data = resource
             });
         }
@@ -104,20 +82,17 @@ namespace DesarrolloTec.API.Controllers
 
         public async Task<IActionResult> Put(Resource resource)
         {
-<<<<<<< HEAD
             _context.Resourcess.Update(resource);
             await _context.SaveChangesAsync();
             return Ok(new
             {
                 message = "Recurso Actualizado con éxito.",
-=======
             _context.Resourcess.Add(resource);
             await _context.SaveChangesAsync();
 
             return Ok(new
             {
                 message = "Recurso Actualizado al proyecto con éxito.",
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
                 data = resource
             });
         }
@@ -126,7 +101,7 @@ namespace DesarrolloTec.API.Controllers
 
         public async Task<IActionResult> Delete(int id)
         {
-<<<<<<< HEAD
+
             var resourceDelete = await _context.Resourcess
                 .Where(x => x.Id == id).ExecuteDeleteAsync();
 
@@ -138,7 +113,6 @@ namespace DesarrolloTec.API.Controllers
             else
             {
                 return Ok(new { message = "Recurso eliminado con éxito." });
-=======
             var resourceDelete = await _context.Resourcess.
                 Where(x => x.Id == id)
                 .ExecuteDeleteAsync();
@@ -150,7 +124,6 @@ namespace DesarrolloTec.API.Controllers
             else
             {
                 return Ok(new { message = "Recurso eliminado con éxito.", data = resourceDelete });
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
             }
         }
     }
