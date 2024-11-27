280911fa958f2ba30fe1e564df5199f23e32fd91
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-11-27 12:32:35 -0500

    resolviendo conflictos

--- DesarrolloTec.API/Controllers/InvoiceController.cs
+++ DesarrolloTec.API/Controllers/InvoiceController.cs
@@ -1,7 +1,6 @@
 ﻿using DesarrolloTec.API.Data;
 using DesarrolloTec.Shared.Entities;
 using DesarrolloTec.Shered.Entities;
-<<<<<<< HEAD
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -14,21 +13,10 @@ namespace DesarrolloTec.API.Controllers
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [ApiController]
     [Route("/api/invoices")]
+
     public class InvoiceController : ControllerBase
     {
 
-=======
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Security.AccessControl;
-
-namespace DesarrolloTec.API.Controllers
-{
-    [ApiController]
-    [Route("/api/invoicecontroller")]
-    public class InvoiceController : ControllerBase
-    {
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
         private readonly DataContext _context;
         public InvoiceController(DataContext context)
         {
@@ -36,8 +24,6 @@ namespace DesarrolloTec.API.Controllers
         }
 
         [HttpGet]
-
-<<<<<<< HEAD
         public async Task<List<Invoice>> GetInvoicesAsync()
         {
             return await _context.Invoices
@@ -46,43 +32,33 @@ namespace DesarrolloTec.API.Controllers
                 .ToListAsync();
         }
 
-=======
         public async Task<IActionResult> Get()
         {
             return Ok(await _context.Invoices.ToListAsync());
         }
 
 
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
         [HttpGet("{id:int}")]
 
         public async Task<IActionResult> Get(int id)
         {
-<<<<<<< HEAD
+
             var invoice = await _context.Invoices
             .Include(i => i.Customers)
             .Include(i => i.Projects)
             .SingleOrDefaultAsync(x => x.Id == id);
-=======
-            var invoice = await _context.Invoices.SingleOrDefaultAsync(x => x.Id == id);
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
+
 
             if (invoice == null)
             {
                 return NotFound();
-<<<<<<< HEAD
-=======
 
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
             }
             else
             {
                 return Ok(invoice);
             }
-<<<<<<< HEAD
-        
-=======
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
+
         }
 
         [HttpPost]
@@ -102,11 +78,11 @@ namespace DesarrolloTec.API.Controllers
 
         public async Task<IActionResult> Put(Invoice invoice)
         {
-<<<<<<< HEAD
+
             _context.Invoices.Update(invoice);
-=======
+
             _context.Invoices.Add(invoice);
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
+
             await _context.SaveChangesAsync();
             return Ok(new
             {
@@ -124,12 +100,8 @@ namespace DesarrolloTec.API.Controllers
 
             if (invoiceDelete == 0)
             {
-<<<<<<< HEAD
-                return NotFound(new { message = "Factura no encontrado." });
-=======
-                return NotFound(new { message = "Factura no encontrada." });
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
 
+                return NotFound(new { message = "Factura no encontrado." });
             }
             else
             {
@@ -138,7 +110,3 @@ namespace DesarrolloTec.API.Controllers
         }
     }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 63af2a287ed4823e7cd5c959bd1201c1c6c8e90a
